4e3d461017693a9dd268364f4c962ccd694e4000
nsawek <snowacoll@hotmail.com>
2018-05-28 20:42:22 +0200

    Start 1
    Dodanie widoków i kontrolerów dla widoku albumów, gatunków, plus połączenie z bazą,

---
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        //
+        public ActionResult Index()
+        {
+            var gen = storeDB.Gen.ToList();
+
+            return View(gen);
+        }
+        //
+
+        //
+        // GET: /Store/Browse?genre=Disco
+        public ActionResult Browse(string gen)
+        {
+            // Pobiera albumy z bazy danych
+            var genreModel = storeDB.Gen.Include("Albumy")
+                .Single(g => g.Name == gen);
+
+            return View(genreModel);
+        }
+        //
+        public ActionResult Details(int id)
+        {
+            var album = storeDB.Albums.Find(id);
+
+            return View(album);
+        }
+    }
+}
---
+++ MvcMusicStore/MvcMusicStore/Models/Album.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+        public string Title { get; set; }
+        public Gen Gen { get; set; }
+        public int AlbumId { get; set; }
+        public int GenreId { get; set; }
+        public int ArtistId { get; set; }
+        public decimal Price { get; set; }
+        public Artist Artist { get; set; }
+        public string AlbumArtUrl { get; set; }
+    }
+}
